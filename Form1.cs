22921123eca489264a88347c6c66f1d04cc29cdc
NerFF <92827001+NerFFx@users.noreply.github.com>
2021-10-22 02:05:11 +0300

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection webcam; 
+
+        private VideoCaptureDevice cam;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webcam = new
+
+           FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo item in webcam)
+
+            {
+
+                comboBox1.Items.Add(item.Name);
+
+            }
+
+            comboBox1.SelectedIndex = 0;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cam = new
+
+           VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString); 
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+            cam.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog swf = new SaveFileDialog();
+
+            swf.Filter = "(*.jpg)|*.jpg|Bitma*p(*.bmp)|*.bmp";
+
+            DialogResult dialog = swf.ShowDialog();
+
+            if (dialog == DialogResult.OK)
+
+            {
+
+                pictureBox1.Image.Save(swf.FileName);
+
+            }
+
+        }
+        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+
+        {
+
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+
+            pictureBox1.Image = bmp;
+
+        }
+
+    }
+}
